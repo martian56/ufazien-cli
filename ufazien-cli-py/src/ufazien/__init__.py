6cb578c41720f17f39e259dc2b2c0f1698d74520
martian56 <fuadelizade469@gmail.com>
2026-08-04 23:49:37 +0400

    chore: bump to 0.3.2

--- ufazien-cli-py/src/ufazien/__init__.py
+++ ufazien-cli-py/src/ufazien/__init__.py
@@ -2,7 +2,7 @@
 Ufazien CLI - Command-line interface for deploying web applications on Ufazien platform.
 """
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 from ufazien.client import UfazienAPIClient
 
